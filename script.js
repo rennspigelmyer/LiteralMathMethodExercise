4428090f79628fd17030a51dafff491f84b47510
rennspigelmyer <catherinespigelmyer95@gmail.com>
2021-09-22 20:42:02 -0400

    completed initial assignment. Have to update after review

---
+++ script.js
@@ -0,0 +1,44 @@
+
+// 1a
+var warmHugs = "Hi, I'm Olaf and I like warm hugs."
+
+// 1b 
+console.log("HI, I'M OLAF AND I LIKE WARM HUGS.")
+
+// 1c
+let like = "love"
+console.log(like.replace("like", "love"));
+//console.log(warmHugs (like.replace("like", "love")));
+//console.log(warmHugs.like)
+//var warmHugs = "Hi, I'm Olaf and I &{like} warm hugs."
+//let warmHugs = warmHugs + like;
+
+
+// 2a 
+var beenImpaled = "Oh, look at that. I've been impaled."
+
+// 2b
+console.log(beenImpaled.slice(17));
+
+// 3a
+var dotDotDot = "..."
+
+// 3b
+var skullBones = `I don't have a skull${dotDotDot}or bones.`
+console.log(skullBones);
+
+// 4
+console.log(Math.PI);
+
+// 5
+let randomNumber = "Math.random()"
+//console.log(randomNumber * (1 - 3));
+//console.log(randomNumber = "Math.random(1-3)");
+//console.log(randomNumber(1,3));
+//randomNumber = Math.random(1,2,3);
+console.log(randomNumber);
+
+
+
+
+// Question for one on one.... Having trouble with understanding how to redeclare/update/reassign variables
