bee0ca3e8cf17262da7f9c212ccaa282b5046462
rennspigelmyer <catherinespigelmyer95@gmail.com>
2021-09-23 10:55:51 -0400

    Updated expercise after review.

--- script.js
+++ script.js
@@ -3,22 +3,26 @@
 var warmHugs = "Hi, I'm Olaf and I like warm hugs."
 
 // 1b 
-console.log("HI, I'M OLAF AND I LIKE WARM HUGS.")
+//console.log("HI, I'M OLAF AND I LIKE WARM HUGS.")
+console.log(warmHugs.toUpperCase())
 
 // 1c
-let like = "love"
-console.log(like.replace("like", "love"));
+//let like = "love"
+//console.log(like.replace("like", "love"));
 //console.log(warmHugs (like.replace("like", "love")));
 //console.log(warmHugs.like)
 //var warmHugs = "Hi, I'm Olaf and I &{like} warm hugs."
 //let warmHugs = warmHugs + like;
+//Above is my attempt. Below is answer.
+console.log(warmHugs.replace("like", "love"));
+console.log(warmHugs);
 
 
 // 2a 
 var beenImpaled = "Oh, look at that. I've been impaled."
 
 // 2b
-console.log(beenImpaled.slice(17));
+console.log(beenImpaled.slice(18));
 
 // 3a
 var dotDotDot = "..."
@@ -31,12 +35,15 @@ console.log(skullBones);
 console.log(Math.PI);
 
 // 5
-let randomNumber = "Math.random()"
+let randomNum = "Math.random()"
 //console.log(randomNumber * (1 - 3));
 //console.log(randomNumber = "Math.random(1-3)");
 //console.log(randomNumber(1,3));
 //randomNumber = Math.random(1,2,3);
-console.log(randomNumber);
+randomNum *= 3;
+randomNum = Math.floor(randomNum);
+randomNum++;
+console.log(randomNum);
 
 
 
